1778e56f584c9cda45e8f97cb1d958fe27a413ca
Magnus Kokk <magnus@kokk.eu>
2023-07-08 21:11:55 +0300

    Don't embed sync.Map so that needed methods are explicitly implemented

--- map.go
+++ map.go
@@ -6,29 +6,29 @@ import (
 
 // syncMapWrapper wraps sync.Map. The zero syncMapWrapper is empty and ready to use.
 type syncMapWrapper[K comparable, V any] struct {
-	sync.Map
+	xmap sync.Map
 }
 
 func (w *syncMapWrapper[K, V]) Load(key K) (value V, ok bool) {
-	if v, ok := w.Map.Load(key); ok {
+	if v, ok := w.xmap.Load(key); ok {
 		return v.(V), true
 	}
 	return *new(V), false
 }
 
 func (w *syncMapWrapper[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
-	if v, ok := w.Map.LoadOrStore(key, value); ok {
+	if v, ok := w.xmap.LoadOrStore(key, value); ok {
 		return v.(V), true
 	}
 	return value, false
 }
 
 func (w *syncMapWrapper[K, V]) Delete(key K) {
-	w.Map.Delete(key)
+	w.xmap.Delete(key)
 }
 
 func (w *syncMapWrapper[K, V]) Range(f func(key K, value V) bool) {
-	w.Map.Range(func(key, value any) bool {
+	w.xmap.Range(func(key, value any) bool {
 		return f(key.(K), value.(V))
 	})
 }
